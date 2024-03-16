b757ce472df957a332e7f688985b830fc1f289ca
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-03-16 14:11:25 +0100

    Added page for Alpine

---
+++ ASPNetWebAppRavenDB/Pages/Alpine.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASPNetWebAppRavenDB.Pages
+{
+    public class AlpineModel : PageModel
+    {
+        private readonly ILogger<AlpineModel> _logger;
+
+        public AlpineModel(ILogger<AlpineModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
